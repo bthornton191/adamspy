0210ac6551c6beaac57941558a1281a07ae2b698
bthornton191 <ben.thornton@mscsoftware.com>
2018-11-20 12:45:34 -0500

    moved the pason filtering function to another package.  Added adamspy.py for generic adams tools

--- adripy.py
+++ adripy.py
@@ -20,135 +20,35 @@ from os import environ
 from os import remove
 from os import rename
 import re
-from scipy import signal
-from numpy import mean
 
 __adrill_user_cfg__ = environ['USERPROFILE'] + '\\.adrill.cfg'
 __adrill_shared_cfg__ = 'C:\\MSC.Software\\Adams\\2018\\adrill\\adrill_shared.cdb'
 __home__ = environ['USERPROFILE']
 
-
 # Dictionary of TO tool length parameters
-TO_length_param = {}
-TO_length_param['accelerator'] = ['Accelerator_Length']
-TO_length_param['agitator'] = ['Power_Body_Length', 'Shock_Stub_Length']
-TO_length_param['blade_reamer'] = ['Reamer_Length']
-TO_length_param['crossover'] = ['Crossover_Length']
-TO_length_param['dart'] = ['Dart_Length']
-TO_length_param['drill_collar'] = ['Drillcollar_Length']
-TO_length_param['drillpipe'] = ['Pipe_Length']
-TO_length_param['flex_pipe'] = ['Flex_Length']
-TO_length_param['generic_long'] = ['Tool_Length']
-TO_length_param['generic_short'] = ['GenericShort_Length']
-TO_length_param['hw_pipe'] = ['Pipe_Length']
-TO_length_param['instrumentation_sub'] = ['ISUB_Length']
-TO_length_param['jar'] = ['Body_Length', 'Stub_Length']
-TO_length_param['mfr_tool'] = ['Tool_Length']
-TO_length_param['motor'] = ['Motor_Length']
-TO_length_param['mwd_tool'] = ['Tool_Length']
-TO_length_param['pdc_bit'] = ['Bit_Length']
-TO_length_param['single_point'] = ['Bit_Length']
-TO_length_param['roller_cone_bit'] = ['Bit_Length']
-TO_length_param['shock_sub'] = ['Installed_Length']
-TO_length_param['short_collar'] = ['Collar_Length']
-TO_length_param['stabilizer'] = ['Stabilizer_Length']
-
-def pason_post_processing(t,sig):
-    """
-    Process a signal as it would be processed by pason
-    
-    Parameters
-    ----------
-    t :      Time series corresponding to the signal to be processed
-    
-    sig :    Signal to be processed
-                  
-    Returns
-    -------
-    t_4, sig_4         Processed time and signal
-    """
-    # (1) Sample at 50 Hz
-    f = 50
-    num = int((t[-1] - t[0])*f + 1)
-    sig_1, t_1 = signal.resample(sig,num,t)
-
-    # (2) 0.99 Hz Digital Low Pass Filter
-    f_c = 0.99
-    f_s = 1/(t_1[1]-t_1[0])
-    W = f_c/f_s*2
-    b,a = signal.butter(5, W)
-    t_2 = t_1
-    sig_2 = list(signal.filtfilt(b, a, sig_1))
-
-    # (3) resample at 5 Hz
-    f = 5
-    num = int((t_2[-1] - t_2[0])*f + 1)
-    sig_3, t_3 = signal.resample(sig_2,num,t_2)
-
-    # (4) calculate maximum over each 1 sec period
-    t_4 = [t_3[0]]
-    sig_4 = [0]
-    counter = 0
-    for i in range(len(t_3)):
-        if counter == 5:
-            t_4.append(t_3[i])
-            sig_4.append(max(sig_3[i-5:i]))
-            counter = 0
-
-        counter += 1
-
-    # set the first value equal to the second value to make the line look nice
-    sig_4[0] = sig_4[1]
-    
-    # (5) calculate mean over each 1 sec period
-    t_5 = [t_3[0]]
-    sig_5 = [0]
-    counter = 0
-    for i in range(len(t_3)):
-        if counter == 5:
-            t_5.append(t_3[i])
-            sig_5.append(mean(sig_3[i-5:i]))
-            counter = 0
-
-        counter += 1
-    # set the first value equal to the second value to make the line look nice
-    sig_5[0] = sig_5[1]
-    
-    # (6) calculate min over each 1 sec period
-    t_6 = [t_3[0]]
-    sig_6 = [0]
-    counter = 0
-    for i in range(len(t_3)):
-        if counter == 5:
-            t_6.append(t_3[i])
-            sig_6.append(min(sig_3[i-5:i]))
-            counter = 0
-
-        counter += 1
-
-    # set the first value equal to the second value to make the line look nice
-    sig_6[0] = sig_6[1]
-
-    # (7) calculate min over each 1 sec period
-    t_7 = [t_3[0]]
-    sig_7 = [0]
-    counter = 0
-    for i in range(len(t_3)):
-        if counter == 5:
-            t_7.append(t_3[i])
-            sig_7.append(sig_3[i])
-            counter = 0
-
-        counter += 1
-
-    # set the first value equal to the second value to make the line look nice
-    sig_7[0] = sig_7[1]
-    
-    # return t_3, sig_3, t_2, sig_2, t_1, sig_1
-    # return [t_7, sig_7, t_6, sig_6, t_5, sig_5, t_4, sig_4, t_3, sig_3, t_2, sig_2, t_1, sig_1]
-    return t_4, sig_4
-    
-
+TO_LENGTH_PARAM = {}
+TO_LENGTH_PARAM['accelerator'] = ['Accelerator_Length']
+TO_LENGTH_PARAM['agitator'] = ['Power_Body_Length', 'Shock_Stub_Length']
+TO_LENGTH_PARAM['blade_reamer'] = ['Reamer_Length']
+TO_LENGTH_PARAM['crossover'] = ['Crossover_Length']
+TO_LENGTH_PARAM['dart'] = ['Dart_Length']
+TO_LENGTH_PARAM['drill_collar'] = ['Drillcollar_Length']
+TO_LENGTH_PARAM['drillpipe'] = ['Pipe_Length']
+TO_LENGTH_PARAM['flex_pipe'] = ['Flex_Length']
+TO_LENGTH_PARAM['generic_long'] = ['Tool_Length']
+TO_LENGTH_PARAM['generic_short'] = ['GenericShort_Length']
+TO_LENGTH_PARAM['hw_pipe'] = ['Pipe_Length']
+TO_LENGTH_PARAM['instrumentation_sub'] = ['ISUB_Length']
+TO_LENGTH_PARAM['jar'] = ['Body_Length', 'Stub_Length']
+TO_LENGTH_PARAM['mfr_tool'] = ['Tool_Length']
+TO_LENGTH_PARAM['motor'] = ['Motor_Length']
+TO_LENGTH_PARAM['mwd_tool'] = ['Tool_Length']
+TO_LENGTH_PARAM['pdc_bit'] = ['Bit_Length']
+TO_LENGTH_PARAM['single_point'] = ['Bit_Length']
+TO_LENGTH_PARAM['roller_cone_bit'] = ['Bit_Length']
+TO_LENGTH_PARAM['shock_sub'] = ['Installed_Length']
+TO_LENGTH_PARAM['short_collar'] = ['Collar_Length']
+TO_LENGTH_PARAM['stabilizer'] = ['Stabilizer_Length']
 
 def turn_measure_on(string_file, tool_types=[], tool_numbers=[], tool_names=[]):
     """
@@ -622,7 +522,7 @@ def get_string_length(string_file):
             fid_tool = open(tool_file, 'r')
             file_type = ''
             for line in fid_tool:
-                if file_type and 'top_drive' not in file_type.lower() and line.replace(' ', '').split('=')[0] in TO_length_param[file_type] and not line.startswith('$'):
+                if file_type and 'top_drive' not in file_type.lower() and line.replace(' ', '').split('=')[0] in TO_LENGTH_PARAM[file_type] and not line.startswith('$'):
                     tool_length = float(line.replace(' ', '').split('=')[1])
                     tool_lengths.append(tool_length)
                     break
@@ -692,7 +592,7 @@ def get_bha_length(string_file):
                 fid_tool = open(tool_file, 'r')
                 file_type = ''
                 for line in fid_tool:
-                    if file_type and 'top_drive' not in file_type.lower() and line.replace(' ', '').split('=')[0] in TO_length_param[file_type] and not line.startswith('$'):
+                    if file_type and 'top_drive' not in file_type.lower() and line.replace(' ', '').split('=')[0] in TO_LENGTH_PARAM[file_type] and not line.startswith('$'):
                         tool_length = float(line.replace(' ', '').split('=')[1])
                         tool_lengths.append(tool_length)
                         break
