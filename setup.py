01ff6b48575e23b9c75cfb77a9ee0cc81bcb792e
bthornton191 <ben.thornton@mscsoftware.com>
2019-02-14 10:53:47 -0600

    measure now type bool in the DrillString.add_tool() method.  Updated doc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="adamspy",
-    version="0.6.15",
+    version="0.6.16",
     author="Ben Thornton",
     author_email="ben.thornton@mscsoftware.com",
     description="Python tools for working with Adams data",
