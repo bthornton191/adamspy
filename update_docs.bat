fd26dd635cf87ceb1c1db99b91ade588f03695b2
bthornton191 <ben.thornton@mscsoftware.com>
2019-02-25 12:34:49 -0500

    changed functions in utitlities to accept cdb alias paths

--- update_docs.bat
+++ update_docs.bat
@@ -4,5 +4,7 @@ sphinx-apidoc -f -o source ..\adamspy
 call make html
 cd ..
 robocopy rst\build\html docs /e /mir
-TIMEOUT  20
+TIMEOUT  5
 pip install adamspy
+TIMEOUT  5
+pip install --upgrade adamspy
