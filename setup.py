583512edc7052e74fa61c0d83027ff40207f1dd6
bthornton191 <ben.thornton@mscsoftware.com>
2019-02-26 12:09:32 -0500

    minor docstring updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="adamspy",
-    version="0.7.8",
+    version="0.7.9",
     author="Ben Thornton",
     author_email="ben.thornton@mscsoftware.com",
     description="Python tools for working with Adams data",
