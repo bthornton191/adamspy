62408ef4ed4fff7c5b547cb577e3857d4ea2c227
bthornton191 <ben.thornton@mscsoftware.com>
2019-02-28 08:51:05 -0500

    adripy.utilities.read_TO_file now reads empty TO tables.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="adamspy",
-    version="0.7.13",
+    version="0.7.14",
     author="Ben Thornton",
     author_email="ben.thornton@mscsoftware.com",
     description="Python tools for working with Adams data",
