7b98dcb2fd638b7eb5ad75db96b8e0698a762e0e
bthornton191 <ben.thornton@mscsoftware.com>
2019-03-05 12:38:16 -0600

    added a get_simdur_from_acf() method to adamspy.utitlities

--- build/lib/adamspy/adamspy.py
+++ build/lib/adamspy/adamspy.py
@@ -25,13 +25,43 @@ def get_simdur_from_msg(msg_file):
     with open(msg_file, 'r') as fid:
         for line in fid:
             if re.match(' *command: sim(ulate)?/dyn(anmic)?.*, *end *=.*', line.lower()):
-                sim_dur = float(re.split('end *=',line.lower().replace(' ',''))[-1].split(',')[0])
-                print(line)
-                print(sim_dur)
+                duration = float(re.split('end *=',line.lower().replace(' ',''))[-1].split(',')[0])
                 found=True
-    if found:
-        return sim_dur
-    else:
+    
+    # Raise an error if no duration found
+    if not found:
         raise RuntimeError('No simulation end time was found in the specified message file!')
+        
+    return duration
 
+def get_simdur_from_acf(acf_file):
+    """Reads an Adams command file (.acf) and returns the total duration of the simulation.
+    
+    Parameters
+    ----------
+    acf_file : str
+        Filepath to an Adams message file (.msg)
+    
+    Raises
+    ------
+    RuntimeError
+        Returned if no simulation end time was found in the specified message file
+    
+    Returns
+    -------
+    float
+        Total duration of the simulation
 
+    """
+    found = False
+    with open(acf_file, 'r') as fid:
+        for line in fid:
+            if re.match('sim(ulate)?/dyn(anmic)?.*, *end *=.*', line.lower()):
+                duration = float(re.split('end *=',line.lower().replace(' ',''))[-1].split(',')[0])
+                found=True
+    
+    # Raise an error if no duration found
+    if not found:
+        raise RuntimeError('No simulation end time was found in the specified message file!')
+        
+    return duration
