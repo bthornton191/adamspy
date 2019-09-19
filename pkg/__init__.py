83df2d5aae1f71ace3b7d4321d8af7ca9e79e491
Ben Thornton <ben.thornton@mscsoftware.com>
2019-09-19 09:16:37 -0400

    increased version numver to upload to pip

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,6 +1,6 @@
 # ---------------------
-version = '0.8.26'
-version_notes = 'uploading to pip'
+version = '0.8.27'
+version_notes = 'increased version numver to upload to pip'
 date = 'September 19th, 2019'
 # ---------------------
 author = 'Ben Thornton'
