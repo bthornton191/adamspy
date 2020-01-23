1e08793a063c15b8219a75e83be7d4ab73670846
Ben Thornton <ben.thornton@mscsoftware.com>
2020-01-23 09:38:25 -0500

    minor bug fixes and dependency updates for vulerability.

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,10 +1,10 @@
 # ---------------------
-version = '0.8.29'
-version_notes = 'minor bug fixes'
-date = 'September 24th, 2019'
+version = '0.9.0'
+version_notes = 'minor bug fixes and dependency updates for vulerability.'
+date = 'January 23rd, 2020'
 # ---------------------
 author = 'Ben Thornton'
 author_email = 'ben.thornton@mscsoftware.com'
 name = 'adamspy'
-install_requires = ['thornpy', 'numpy', 'pandas', 'Jinja2', 'dripy', 'scipy']
+install_requires = ['thornpy', 'numpy', 'pandas', 'Jinja2', 'dripy', 'scipy', 'matplotlib']
 description = 'Python tools for working with Adams data'
