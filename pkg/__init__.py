1892ea5931dc672e653101962ebdf4c967e74593
Ben Thornton <ben.thornton@mscsoftware.com>
2020-08-31 10:30:39 -0400

    added function to modify the path to the .xmt_txt file in an Adams View command (.cmd) file.

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,7 +1,7 @@
 # ---------------------
-version = '0.24.0'
+version = '0.25.0'
 version_notes = 'added function to modify the path to the .xmt_txt file in an Adams View command (.cmd) file.'
-date = 'July 20th, 2020'
+date = 'August 31st, 2020'
 # ---------------------
 author = 'Ben Thornton'
 author_email = 'ben.thornton@mscsoftware.com'
