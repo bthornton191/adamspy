a682a5107050dc436b82e56dea0e6f9181ac02bb
Ben Thornton <ben.thornton@hexagon.com>
2022-08-25 15:41:12 -0400

    merging

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,10 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-<<<<<<< HEAD
-    VERSION: '0.29.8',
-=======
-    VERSION: '0.29.10',
->>>>>>> 4b9165c62d689cf5ae34e79ed1508db859439cad
+    VERSION: '0.29.12',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
